15ddf9305d80294005142eb3f360dc7025ec53b0
Lorant98 <72556800+Lorant98@users.noreply.github.com>
2022-05-21 19:01:07 +0200

    Update 41_First_Missing_Positive.cs

--- 41_First_Missing_Positive.cs
+++ 41_First_Missing_Positive.cs
@@ -1,1 +1,24 @@
-﻿
+﻿namespace LeetCode_41
+{
+    class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int j = nums[i];
+
+                while (j > 0 && j <= nums.Length && nums[j - 1] != j)              
+                    (j, nums[j - 1]) = (nums[j - 1], j);                
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1)
+                    return i + 1;
+            }
+
+            return nums.Length + 1;
+        }
+    }
+}
