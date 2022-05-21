5cb70b490579539aa440774db924f32583fec714
Lorant98 <72556800+Lorant98@users.noreply.github.com>
2022-05-21 18:53:43 +0200

    Add files via upload

---
+++ 780_Reaching_Points.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode_780
+{
+    class Solution
+    {
+        public bool ReachingPoints(int sx, int sy, int tx, int ty)
+        {
+            while (true)
+            {
+                if (sx > tx || sy > ty)
+                    return false;
+
+                if (sx == tx)
+                    return (ty - sy) % sx == 0;
+
+                if (sy == ty)
+                    return (tx - sx) % sy == 0;
+
+                if (tx >= ty)
+                    tx %= ty;
+                else
+                    ty %= tx;
+            }
+        }
+    }
+}
